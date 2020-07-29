338dad7c3ee9045da9ce591db87dc5079ab361d0
Newton Scavazzini <newtonscavazzini@gmail.com>
2020-07-29 03:46:34 -0300

    Create podcast_ripper package

---
+++ podcast_ripper/__init__.py
@@ -0,0 +1,8 @@
+from podcast_ripper import util
+from podcast_ripper.podcast import Podcast
+from podcast_ripper.episode import Episode
+
+parse = util.parse
+
+__version__ = '0.0.1dev'
+__user_agent__ = 'Mozilla/5.0 (X11; Linux x86_64)'
