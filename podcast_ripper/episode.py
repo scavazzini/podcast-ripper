22694edbeecae0bab3a0561a5973eed182f8c8d9
Newton Scavazzini <newtonscavazzini@gmail.com>
2020-07-29 03:44:09 -0300

    Create Episode class

---
+++ podcast_ripper/episode.py
@@ -0,0 +1,34 @@
+import os
+
+import requests
+from slug import slug
+
+import podcast_ripper
+
+
+class Episode:
+
+    def __init__(self, title, published, file_url):
+        self.title = title
+        self.published = published
+        self.file_url = file_url
+
+    def download(self, location=''):
+
+        request = requests.get(self.file_url, headers={"User-Agent": podcast_ripper.__user_agent__})
+        content = request.content
+
+        path = os.path.join(location, self.filename)
+
+        with open(path, 'wb') as f:
+            f.write(content)
+
+        return len(content)
+
+    @property
+    def filename(self):
+        return "{}-{}.{}".format(self.published, slug(self.title), self.extension)
+
+    @property
+    def extension(self):
+        return self.file_url.split('.')[-1].lower()
