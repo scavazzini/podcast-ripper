84590be72a57b91734819c08d759294619eb1617
Newton Scavazzini <newtonscavazzini@gmail.com>
2020-07-29 12:23:59 -0300

    Extract downloadPodcast method

--- podcast_ripper/cli.py
+++ podcast_ripper/cli.py
@@ -19,10 +19,31 @@ def cli():
 def download(feed_url, destination, max_episodes):
     """Download episodes"""
 
-    print('Fetching feed...')
+    feeds = [feed_url]
+    destination = destination or ''
+    downloaded = 0
 
-    podcast = podcast_ripper.parse(feed_url, max_episodes)
-    download_dir = os.path.join(destination or '', slug(podcast.name))
+    try:
+
+        for feed in feeds:
+
+            print('Fetching feed...')
+            podcast = podcast_ripper.parse(feed, max_episodes)
+            download_dir = os.path.join(destination, slug(podcast.name))
+
+            downloaded += downloadPodcast(podcast, download_dir)
+
+    except KeyboardInterrupt:
+        print()
+        print("Aborted. {} downloaded.".format(humanize.naturalsize(downloaded)))
+        exit()
+
+    print()
+    print("Completed. {} downloaded.".format(humanize.naturalsize(downloaded)))
+    print()
+
+
+def downloadPodcast(podcast, download_dir):
 
     print()
     print("{} ({})".format(podcast.name, podcast.url))
@@ -34,24 +55,17 @@ def download(feed_url, destination, max_episodes):
     downloaded = 0
     w = len(str(len(podcast.episodes)))
 
-    try:
+    for i, episode in enumerate(podcast.episodes, 1):
 
-        for i, episode in enumerate(podcast.episodes, 1):
+        if os.path.exists(os.path.join(download_dir, episode.filename)):
+            print(" [{0:{w}d}/{1}] {2} already exists".format(i, len(podcast.episodes), episode.filename, w=w))
+            continue
 
-            if os.path.exists(os.path.join(download_dir, episode.filename)):
-                print(" [{0:{w}d}/{1}] {2} already exists".format(i, len(podcast.episodes), episode.filename, w=w))
-                continue
-
-            print(" [{0:{w}d}/{1}] Downloading {2}".format(i, len(podcast.episodes), episode.filename, w=w))
-            downloaded += episode.download(download_dir)
-
-    except KeyboardInterrupt:
-        print()
-        print("Aborted. {} downloaded.".format(humanize.naturalsize(downloaded)))
-        exit()
+        print(" [{0:{w}d}/{1}] Downloading {2}".format(i, len(podcast.episodes), episode.filename, w=w))
+        downloaded += episode.download(download_dir)
 
     print()
-    print("Completed. {} downloaded.".format(humanize.naturalsize(downloaded)))
+    return downloaded
 
 
 if __name__ == "__main__":
